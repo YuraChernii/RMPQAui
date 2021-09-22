5fc6d403276604d49f274a09aaf99cc17981e238
Yurii Chernii <yurii.chernii.alpha@gmail.com>
2021-09-22 10:19:05 +0300

    added request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpXhrBackend} from '@angular/common/http';
+import { User } from '../app/Models/User';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RMPQAui';
+  user: User | undefined;
+httpClient2 = new HttpClient(new HttpXhrBackend({ build: () => new XMLHttpRequest() }));
+    constructor(){}
+      
+    ngOnInit(){
+          
+        this.httpClient2.get('https://localhost:5001/Product').subscribe((data:any) => data.data);
+    }
 }
